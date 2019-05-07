c385c2817e77f47de5c6b81760f87e09dc006feb
Tony Chen <tonyc856@users.noreply.github.com>
2019-05-07 13:09:08 -0700

    Fixed issue with navbar cutting off pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
 import Visualization from './components/Visualization/Visualization';
 import About from './components/About';
-
 import NavBar from './components/NavBar/NavBar';
 import './App.scss';
 
-export default class App extends Component {
+const styles = (theme) => ({
+  toolbar: theme.mixins.toolbar,
+});
+
+class App extends Component {
 
   render() {
+    const { classes } = this.props;
     return (
       <Router>
         <div className="App">
           <NavBar />
-          <div className="App-body">
-            <Route path="/" exact component={Visualization} />
-            <Route path="/about" component={About} />
-          </div>
+          <Paper>
+            <div className={classes.toolbar} />
+            <div className="App-body">
+              <Route path="/" exact component={Visualization} />
+              <Route path="/about" component={About} />
+            </div>
+          </Paper>
         </div>
       </Router>
     );
   }
 }
+
+export default withStyles(styles)(App);
