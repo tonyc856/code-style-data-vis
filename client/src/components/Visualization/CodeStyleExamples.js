6b63e80e766209940b4baad10e81474823db00ce
Tony Chen <tonyc856@users.noreply.github.com>
2019-05-19 18:44:03 -0700

    Added visual icons along with code exmaple headings.

--- client/src/components/Visualization/CodeStyleExamples.js
+++ client/src/components/Visualization/CodeStyleExamples.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { ThumbUp, ThumbDown } from '@material-ui/icons';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
 
@@ -10,9 +11,16 @@ export default class CodeStyleExamples extends Component {
     return (
       <Grid item container id="code-style-examples" justify="flex-start" direction="column">
         <Grid item>
-          <Typography variant="subheading" color="inherit">
-              Good: 
-          </Typography>
+          <Grid container justify="flex-start" direction="row">
+            <Grid item>
+              <ThumbUp className="icon valid-color" />
+            </Grid>
+            <Grid item>
+              <Typography variant="subheading" color="inherit">
+                  Good Example: 
+              </Typography>
+            </Grid>
+          </Grid>
           <SyntaxHighlighter
               className="code-example"
               language={ this.props.language }>
@@ -20,9 +28,16 @@ export default class CodeStyleExamples extends Component {
           </SyntaxHighlighter>
         </Grid>
         <Grid item>
-          <Typography variant="subheading" color="inherit">
-            Bad: 
-          </Typography>
+        <Grid container justify="flex-start" direction="row">
+          <Grid item>
+              <ThumbDown className="icon" color="error" />
+            </Grid>
+            <Grid item>
+              <Typography variant="subheading" color="inherit">
+                  Bad Example: 
+              </Typography>
+            </Grid>
+          </Grid>
           <SyntaxHighlighter
             className="code-example"
             language={ this.props.language }>
